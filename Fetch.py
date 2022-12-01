f9569ab994b5ad5def38ff8d284a005066ec87c8
KristinSB <burkhardt.k1@gmail.com>
2022-11-30 20:15:25 -0500

    Dog Training

---
+++ Fetch.py
@@ -0,0 +1,19 @@
+from breezypythongui import EasyFrame
+
+“””
+
+Fetch! Dog Training
+
+This application will request the user to input some data for dog training
+
+“””
+
+ 
+
+class FetchTng(EasyFrame):
+
+ 
+
+ 
+
+FetchTng().MainLoop()
